6f70c8919c9fb83f3760cf86a7adad8b33c3769b
kubano <jakubno3ak@gmail.com>
2025-03-24 20:30:49 +0100

    Add users management 2

--- aws/terraform/users_management/users.tf
+++ aws/terraform/users_management/users.tf
@@ -7,6 +7,21 @@ resource "aws_iam_user" "users" {
   name     = each.value
 }
 
+resource "aws_iam_user_login_profile" "users" {
+  for_each        = aws_iam_user.users
+  user            = each.value.name
+  password_length = 8
+
+  lifecycle {
+    # ENSURES THAT ANY CHANGES HERE WILL NOT IMPACT EXISTING USERS
+    ignore_changes = [
+      password_length,
+      password_reset_required,
+      pgp_key
+    ]
+  }
+}
+
 output "users" {
   value = local.users_from_yaml
 }
