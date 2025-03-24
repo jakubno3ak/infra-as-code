beae7e7fb6400ea368e3a03d5c9ccaba45ac3d55
kubano <jakubno3ak@gmail.com>
2025-03-24 20:44:49 +0100

    Add users management 3

---
+++ aws/terraform/users_management/roles.tf
@@ -0,0 +1,35 @@
+locals {
+  role_policies = {
+    readonly = [
+      "ReadOnlyAccess"
+    ]
+    admin = [
+      "AdministratorAccess"
+    ],
+    auditor = [
+      "SecurityAudit"
+    ]
+    developer = [
+      "AmazonVPCFullAccess",
+      "AmazonEC2FullAccess",
+      "AmazonRDSFullAccess"
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "AWS"
+      identifiers = [""]
+    }
+  }
+}
+
+resource "aws_iam_role" "roles" {
+  for_each = toset(keys(local.role_policies))
+  name     = each.key
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+}
