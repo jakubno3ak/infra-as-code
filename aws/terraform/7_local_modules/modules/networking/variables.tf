ac895d666d71b749a2b04a3e62a8311669ae1da4
kubano <jakubno3ak@gmail.com>
2025-03-27 20:24:25 +0100

    Add local module

---
+++ aws/terraform/7_local_modules/modules/networking/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_config" {
+  type = object({
+    cidr_block = string
+    name       = string
+  })
+
+  validation {
+    condition     = can(cidrnetmask(var.vpc_config.cidr_block))
+    error_message = "The variable vpc_cidr must contain a valid cidr block."
+  }
+}
