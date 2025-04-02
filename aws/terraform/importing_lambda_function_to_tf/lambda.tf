cc840e4229c044a655022dfa5cd1434c0966f534
kubano <jakubno3ak@gmail.com>
2025-04-02 21:51:00 +0200

    Import manually created lambda

---
+++ aws/terraform/importing_lambda_function_to_tf/lambda.tf
@@ -0,0 +1,30 @@
+import {
+  to = aws_lambda_function.this
+  id = "manually-created-lambda"
+}
+
+data "archive_file" "lambda_code" {
+  type        = "zip"
+  source_file = "${path.root}/build/lambda_function.py"
+  output_path = "${path.root}/lambda.zip"
+}
+
+resource "aws_lambda_function" "this" {
+  description      = "A starter AWS Lambda function."
+  filename         = "lambda.zip"
+  function_name    = "manually-created-lambda"
+  handler          = "lambda_function.lambda_handler"
+  role             = "arn:aws:iam::634993322111:role/service-role/manually-created-lambda-role-cj5sxk6d"
+  runtime          = "python3.10"
+  source_code_hash = data.archive_file.lambda_code.output_base64sha256
+
+  tags = {
+    "lambda-console:blueprint" = "hello-world-python"
+  }
+
+  logging_config {
+    application_log_level = null
+    log_format            = "Text"
+    log_group             = "/aws/lambda/manually-created-lambda"
+  }
+}
